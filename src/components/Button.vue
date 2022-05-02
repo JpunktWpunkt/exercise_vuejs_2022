b57296c129608029d67345b9dd2037b816bfce93
JpunktWpunkt <wittig.jessica@googlemail.com>
2022-05-02 05:45:27 +0200

    add new components and local files to built the structure for an TaskTracker

---
+++ src/components/Button.vue
@@ -0,0 +1,19 @@
+<template>
+  <button @click="onClick()" :style="{background: color}" class="btn">{{ text }}</button>
+<!--  v-on:click="onClick()" oder einfach @click-->
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    text: String,
+    color: String
+  },
+  methods:{
+    onClick(){
+      console.log('Click')
+    }
+  }
+}
+</script>
